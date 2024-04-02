8e3bc584202809dd4632fe924a6141d8a1509068
Boris Marinov <marinovboris@protonmail.com>
2024-04-02 12:29:39 +0300

    stuff

--- interpreter/pattern-match.js
+++ interpreter/pattern-match.js
@@ -10,20 +10,22 @@ const patternMatchArgumentsByNames = (names, args, env, exec) => {
     //if (debug) console.log(`with environment`, env.functions)
   }
 
-  if (names.length !== args.length) {
-    return error(`Expected a ${names.length} arguments, but got ${args.length}`, env, {names, args})
-  } else {
-    const argumentEnv = names.reduce((argumentEnv, name, i) => {
-
-      // Check for errors
-    
-      let value = args[i]
+  const argumentEnv = {}
+  for (let i = 0; i < names.length; i++) {  
+    let name = names[i]
+    let value = args[i]
+    if (value === undefined){
+      console.log({names, args})
+      throw Error(`No value supplied for ${formatExpression(name)}`)
+    } else if (false) {
+      //TODO check if name === func.name (results in infinite loop)
+    } else {
        if (debug) console.log(`Matching expression ${formatExpression(name)} with value ${formatExpression(value)}`);
       if (!Array.isArray(name)) {
         // When we encounter data constructors,
         // we pattern-match it and add the elements
         //argumentEnv[name] = [{ args: [], expression: value, env}]
-        throw 'System error: Invalid signature'
+        throw Error('System error: Invalid signature')
 
       // Normal argument
       // We pattern-match it and add it to the environment
@@ -73,28 +75,21 @@ const patternMatchArgumentsByNames = (names, args, env, exec) => {
           throw Error(`Wrong use of the :lambda keyword, called with ${literal.length} arguments, but expected 1.`)
         }
 
-      /*
-      } else if (name[0] === ':rest') {
+      } else if (name[0] === ':list') {
+        debug && console.log(`Executing a :list expression at index ${i}`)
         if (name.length === 2) {
           const nameExecuted = exec(name[1])
           if (nameExecuted.length === 1) {
             const argumentName = nameExecuted[0]
-            // If the argument is a function that is already defined, 
-            // search for it's implementation and attach it.
-            if (value.length === 1) {
-              const implementation = value.env.functions[value[0]]
-              argumentEnv[functionName] = implementation
-            } else {
-            // If the function is given inline, execute it
-              argumentEnv[functionName] = exec(value)
-            }
+            debug && console.log(`Assigning ${argumentName} to ${formatExpression(args.slice(i))}`)
+            argumentEnv[argumentName] = [{ args: [], expression: ['List'].concat(args.slice(i)), env}]
+            break
           } else {
-            throw Error(`Pattern-matching on :rest expressions is not supported... yet.`)
+            throw Error(`Pattern-matching on :list expressions is not supported.`)
           }
         } else {
-          throw Error(`Wrong use of the :lambda keyword, called with ${literal.length} arguments, but expected 1.`)
+          throw Error(`Wrong use of the :list keyword, called with ${literal.length} arguments, but expected 1.`)
         }
-      */
 
       // Data structure
       // We destruct it and match it's contents recursively
@@ -105,8 +100,6 @@ const patternMatchArgumentsByNames = (names, args, env, exec) => {
         // Typecheck 
         if (nameType !== argType) {
           return error(`Expected a type '${nameType}', but got ${argType}`, env, {name, value})
-        } else if (nameVals.length !== argVals.length) {
-          return error(`Expected a ${nameVals.length} arguments , but got ${argVals.length} in a pattern match ${formatExpression(name)}`, env, {nameVals, argVals})
         } else {
           if (debug) console.log(`Assigning values ${formatExpression(nameVals)} with values ${formatExpression(argVals)}`);
           //TODO report errors from here 
@@ -114,12 +107,11 @@ const patternMatchArgumentsByNames = (names, args, env, exec) => {
           Object.assign(argumentEnv, patternMatchArgumentsByNames(nameVals, argVals, env, exec))
         }
       }
-      return argumentEnv
-    }, {})
-
-    if (debug && Object.keys(argumentEnv).length > 0) console.log("Constructed argument environment", argumentEnv)
-    return argumentEnv
+    }
   }
+  if (debug && Object.keys(argumentEnv).length > 0) console.log("Constructed argument environment", argumentEnv)
+
+  return argumentEnv
 }
 
 module.exports = {patternMatchArgumentsByNames}
